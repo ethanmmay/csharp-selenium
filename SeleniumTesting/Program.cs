e2e3dfff244a373616ed1b2e845d9f0a6fe75ce4
Ethan May <ethaniel.may@gmail.com>
2022-05-15 21:47:05 -0600

    Full Test Case

--- SeleniumTesting/Program.cs
+++ SeleniumTesting/Program.cs
@@ -12,10 +12,25 @@ namespace SeleniumTesting
     {
         static void Main(string[] args)
         {
+            Console.Write("Test Case Started");
             IWebDriver driver = new ChromeDriver();
 
             driver.Navigate().GoToUrl("https://www.google.com");
+            Thread.Sleep(2000);
+
             driver.Manage().Window.Maximize();
+            IWebElement ele1 = driver.FindElement(By.Name("q"));
+
+            ele1.SendKeys("javatpoint tutorials");
+            Thread.Sleep(2000);
+
+            IWebElement ele2 = driver.FindElement(By.Name("btnK"));
+
+            ele2.Click();
+            Thread.Sleep(3000);
+
+            driver.Close();
+            Console.Write("Test Case Ended");
         }
     }
 }
